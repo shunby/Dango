4cd71c411cabb39742f823a700f4a7bc4c1de6ac
shunby <taby@outlook.jp>
2017-11-07 21:22:50 +0900

    post デザイン修正

--- post/index.php
+++ post/index.php
@@ -17,6 +17,7 @@
   <link href="../template/main.css" rel="stylesheet" type="text/css">
   <link href="../template/content.css" rel="stylesheet" type="text/css">
   <link href="../template/navi.css" rel="stylesheet" type="text/css">
+  <link href="posts.css" rel="stylesheet" type="text/css">
   <link href="" rel="shortcut icon">
   <!--[if lt IE 9]>
   <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
@@ -34,7 +35,7 @@
         <p>
           記事を投稿したい場合は<a href="/questionnaire.php">お問い合わせ</a>から。
         </p>
-        <ul>
+        <ul id="posts">
           <?php include "posts.html" ?>
         </ul>
 
