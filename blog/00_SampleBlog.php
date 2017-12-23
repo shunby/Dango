35bdfe92a632e9665e8b020345a47c8f1afc16bf
BlackDesert <kokusyunn@gmail.com>
2017-12-23 19:20:17 +0900

    いいねシステム　途中経過

--- blog/00_SampleBlog.php
+++ blog/00_SampleBlog.php
@@ -50,30 +50,6 @@
           ね、簡単でしょう?
         </p>
       </div>
-      <input type="button" value="good" id="button"><!--ボタン-->
-      <a><?php
-      $number = file_get_contents("ajax.txt");
-      echo $number;
-      ?></a>
-      <script>
-        $(function(){
-          $('#button').click(
-            function(){
-              $.ajax({
-                type: 'POST',
-                url: 'ajax.php',
-              })
-              .then(
-                function(result){
-                  $("a").text(result);
-                },
-                function(){
-                  ;
-                });
-               }
-              );
-            });
-      </script>
       <!--ここまで本文-->
 
 
