4fcaa3017d051bd6d0f90618e8a79519e20d71d0
iamolaf <kkitagawa0212@gmail.com>
2017-12-24 22:57:01 +0900

    いろいろ

---
+++ olaf/olafblog02.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>だんご三兄弟</title>
+
+  <meta name="description" content="">
+  <meta name="author" content="だんご三兄弟">
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <?php
+    $webroot = $_SERVER['DOCUMENT_ROOT'];
+    include $webroot."/template/analytics.html"
+   ?>
+  <link href="index.css" rel="stylesheet" type="text/css">
+  <link href="/template/header.css" rel="stylesheet" type="text/css">
+  <link href="/template/footer.css" rel="stylesheet" type="text/css">
+  <link href="/template/main.css" rel="stylesheet" type="text/css">
+  <link href="/template/content.css" rel="stylesheet" type="text/css">
+  <link href="/template/navi.css" rel="stylesheet" type="text/css">
+  <link href="" rel="shortcut icon">
+  <!--[if lt IE 9]>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
+  <![endif]-->
+</head>
+<body>
+  <p>
+  どうも！おらふのブログのお時間です‼<br>
+  いくつかネタを提供していただきましたが、今回は募集したネタは使いません。<br>
+  今回はとりあえず今後のおらふのブログの予定についてお話しできたらな、と思います。<br>
+  これからは「宿題」という物を作ろうと思います。<br>
+  「宿題」は、あるテーマについて私おらふが意見を募集し、金曜日に個人的に面白いと思ったものを発表させていただきます<br>
+  とりあえず更新は月水金日の週四日にします。それぞれの更新内容は、<br>
+  月　募集したネタ<br>
+  水　募集したネタ＆金曜までの宿題<br>
+  金　集まった宿題をまとめてランキングにして発表<br>
+  日　おらふ的一週間のネットニュースまとめ<br>
+  にします‼<br>
+  とりあえず今回は早めに宿題を出してみます<br>
+  （宿題）　東京オリンピックのとんでもない新種目を考えてください<br>
+  宿題の答えの投稿は<a href="/bbs/messages.php?roomid=0&threadid=3">おらふのブログネタ募集スレ</a>から
+ </p>
+</body>
+</html>
