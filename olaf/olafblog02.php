c2f21c12c23e2222f0d7070a96f3db0abe438cf6
iamolaf <kkitagawa0212@gmail.com>
2017-12-25 22:55:34 +0900

    変更

--- olaf/olafblog02.php
+++ olaf/olafblog02.php
@@ -41,7 +41,7 @@
   にします‼<br>
   とりあえず今回は早めに宿題を出してみます<br>
   （宿題）　東京オリンピックのとんでもない新種目を考えてください<br>
-  宿題の答えの投稿は<a href="/bbs/messages.php?roomid=0&threadid=3">おらふのブログネタ募集スレ</a>から
+  宿題の答えの投稿は<a href="http://paysestbbs.webcrow.jp/bbs/threads.php?roomid=4">おらふの部屋</a>から
  </p>
 </body>
 </html>
