2e34e703bf162378ed751dc7db1059d194d94aac
shunby <taby@outlook.jp>
2017-12-30 16:48:46 +0900

    オラフのページにログインチェック機構を導入

--- olaf/index.php
+++ olaf/index.php
@@ -1,3 +1,8 @@
+<?php
+  $webroot = $_SERVER['DOCUMENT_ROOT'];
+  include $webroot."/template/check_login.php"
+ ?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -39,7 +44,7 @@
            日々のネタを不定期更新する「おらふの日常」は<a href="dailyofolaf.php">こちら</a><br>
            ネットの話題をお届けする「おらふのネット情報局」は<a href="informationblog.php">こちら</a>
          </p>
-         
+
 
      </section>
    </article>
