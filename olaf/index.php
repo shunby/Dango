8d205eaf62c8df15190e74747593634177a6a606
iamolaf <kkitagawa0212@gmail.com>
2017-12-30 11:58:42 +0900

    変更

--- olaf/index.php
+++ olaf/index.php
@@ -42,18 +42,24 @@
       <section>
         <h1>おらふのブログ</h1>
         <p>
-        運営メンバーおらふによるおらふのブログ！<br>
-        日々様々な情報をお伝えします<br>
-        ・<a href="dailyofolaf.php">おらふの日常</a><br>
-        ・<a href="informationblog.php">おらふのネット情報局</a><br>
-        </p>
 
-      </section>
-    </article>
+おらふのブログのアップデートのため数日間更新をストップします<br>
+       申し訳ございません<br>
+       アップデートの詳しい情報は<a href="/olaf/updateinformation.php">こちら</a><br>
+       宿題の投稿は<a href="http://paysestbbs.webcrow.jp/bbs/messages.php?roomid=4&threadid=1">おらふのブログネタ募集スレ</a>から<br>
+     </p>
+       <ul>
+         <li><a href="/olaf/olafblog01.php">第一回</li>
+         <li><a href="/olaf/olafblog02.php">第二回</li>
+         <li><a href="/olaf/olafblog03.php">第三回</li>
+       </ul>
+>>>>>>> master
+     </section>
+   </article>
 
-    <?php include $webroot."/template/footer.html" ?>
+   <?php include $webroot."/template/footer.html" ?>
 
-  </div>
+ </div>
 </body>
 
 </html>
