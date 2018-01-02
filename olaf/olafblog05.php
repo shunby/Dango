9c6093a918f67b160586c224c0f3f7c5dc751e4a
iamolaf <kkitagawa0212@gmail.com>
2018-01-02 23:38:55 +0900

    ０５

---
+++ olaf/olafblog05.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>だんご三兄弟</title>
+
+  <meta name="description" content="">
+  <meta name="author" content="だんご三兄弟">
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <?php
+    $webroot = $_SERVER['DOCUMENT_ROOT'];
+    include $webroot."/template/analytics.html"
+   ?>
+  <link href="index.css" rel="stylesheet" type="text/css">
+  <link href="/template/header.css" rel="stylesheet" type="text/css">
+  <link href="/template/footer.css" rel="stylesheet" type="text/css">
+  <link href="/template/main.css" rel="stylesheet" type="text/css">
+  <link href="/template/content.css" rel="stylesheet" type="text/css">
+  <link href="/template/navi.css" rel="stylesheet" type="text/css">
+  <link href="" rel="shortcut icon">
+  <!--[if lt IE 9]>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
+  <![endif]-->
+</head>
+<body>
+
+
+  <div id="content">
+    <?php include $webroot."/template/header.html" ?>
+    <?php include $webroot."/template/navi.html" ?>
+    <main id="main">
+    <p>
+      どうも皆さんお待ちかね（？）のおらふのブログのお時間です‼<br>
+      先日は楽しいニート生活を送っていて、どーもすみませんでした。ねこさん、ありがとう。<br>
+      早速ですが、大変長らくお待たせいたしました、宿題の発表です<br>
+      今回の宿題は、「東京オリンピックの新種目」でした！それでは、どうぞ！<br>
+      （PN＝ペンネーム）<br>
+    </p>
+    <p>
+     <h3>最優秀賞</h3>
+      PN:シャム　回答：オフ会で何人人が来ないか選手権<br>
+      評議委員からひと言：ペンネームの通り、某人気底辺ユーチューバーsyamuへの大きな愛が感じられる回答です。私自身、syamuのことは大好きでして、
+      復活を望んでいる者の一人であります。今回は私の愛するsyamuへの敬意をこめて独断と偏見で最優秀賞とさせていただきました。<br>
+    </p>
+    <p>
+      <h3>優秀賞</h3>
+      PN:温ぴの友達の遠い親戚　回答：10秒間に何回えびらを叩けるか選手権<br>
+      評議委員からひと言：ぶっちゃけ訳が分かりません。こんなことをして何になるのでしょうか。
+      念のためもう一度言います。訳が分かりません。でもまあ、面白いのでアリです。<br>
+      ひとつ言わせていただくと、私自身その競技の練習風景を見たことがあるのですが、あれは果たして「叩く」と言えるのでしょうか？
+      私には「揉んで」いたように見えます<br>
+    </p>
+    <p>
+      <h3>佳作</h3>
+      PN:福岡卍共和国　回答：漫才<br>
+      PN:いたがき　回答：定期試験でどれだけ0点に近づけるかの度胸試し<br>
+    </p>
+    <p>
+      今回は10件の応募をいただきました！4割の確率で入選とかそういうこと言わない((<br>
+      今回は初回だったので多くしましたが、次からは減らします<br>
+      今回入選された方は、ユーザーページ→お知らせから運営までお知らせください。そんなに期待できない報酬が待ってます。<br>
+      ここで、次の宿題。<br>
+      次の宿題は、「ごく普通のことを名言っぽく言ってください」です！<br>
+      例）「初対面の時思ったんだ。まるで…初めて会ったみたいだ。って」<br>
+      宿題の投稿は<a href="http://paysestbbs.webcrow.jp/bbs/messages.php?roomid=4&threadid=2">こちら</a><br>
+    </p>
+    <p>
+      そして今回の話題。<br>
+      おらふのパソコンについて。<br>
+      スペック↓↓↓<br>
+      CPU　intel core-i7　7700k<br>
+      GPU NVIDIA  GeForce GTX1070<br>
+      SSD Crucial MX300 525GB<br>
+      HDD SEAGATE BarraCuda 1TB<br>
+      まあ気になったら調べてみてください。ぶっちゃけハイスぺです。<br>
+      そしてそしてそして<br>
+      ネタ募集のところに「ゲーム実況してください！なんでもしますから！」とか書いてあったのでね<br>
+　　　ゲーム実況でもしてみようかと思います。もちろんゆっくりボイスです。<br>
+　　　と、いうことで。<br>
+     ネタ募集スレに実況してもらいたいゲームを書いといてください。<br>
+     予算の都合上、「Minecraft」「BattleField 4」「World of Tanks Blitz」もしくは無料のゲームにしてください。<br>
+     （もしくは、ゲーム購入資金をください）<br>
+     多分スペックの点では問題ないと思います。エロゲは無理です。自分一人で楽しむものでしょ、あれって。<br>
+     だんだん眠くなってきたので、今日はここまで。<br>
+     では皆さん、明日は情報局でお会いしましょう。ネタは、「炎上したバカッターの末路」です<br>
+     どうぞお楽しみに。<br>
+   </p>
+
+ <?php include $webroot."/template/footer.html" ?>
+</body>
+</html>
