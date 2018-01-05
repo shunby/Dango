d3a867e1e0264fd4b6a0cbdddf567bd58065368f
BlackDesert <kokusyunn@gmail.com>
2018-01-05 21:47:29 +0900

    blogサンプル修正

--- blog/00_SampleBlog.php
+++ blog/00_SampleBlog.php
@@ -18,10 +18,11 @@
     <link href="/template/blog/blog.css" rel="stylesheet" type="text/css">
     <link href="/template/header.css" rel="stylesheet" type="text/css">
     <link href="/template/footer.css" rel="stylesheet" type="text/css">
-    <link href="/template/main.css" rel="stylesheet" type="text/css">
+    <link href="/template/sidemain.css" rel="stylesheet" type="text/css">
     <link href="/template/content.css" rel="stylesheet" type="text/css">
     <link href="/template/navi.css" rel="stylesheet" type="text/css">
     <link href="/template/iine/ajax.css" rel="stylesheet"  type="text/css">
+    <link rel="stylesheet" href="/template/sideber.css" type="text/css">
     <link href="" rel="shortcut icon">
     <!--[if lt IE 9]>
     <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
@@ -36,30 +37,34 @@
         include $webroot."/template/header.html"
       ?>
       <?php include $webroot."/template/navi.html" ?>
-      <?php include $webroot."/template/blog/before.php" ?>
 
-      <?php
-        $id = 0;
-       ?>
+      <div id="sidemain">
+        <?php include $webroot."/template/blog/before.php" ?>
 
-      <!--ここから本文-->
-      <div class="main-main">
-        <p>
-          これはブログ記事のテストです。<br>
-        </p>
-        <p>
-          かといって適当なことを書くのもアレなので、このテンプレートの使い方を書いていきます。<br>
-          でも、そこまで難しくはないです。ブログidの設定をして、blogs.xmlにブログの情報を書き込むだけです。<br>
-        </p>
-        <p>
-          ね、簡単でしょう?
-        </p>
+        <?php
+          $id = 0;
+         ?>
+
+        <!--ここから本文-->
+        <div class="main-main">
+          <p>
+            これはブログ記事のテストです。<br>
+          </p>
+          <p>
+            かといって適当なことを書くのもアレなので、このテンプレートの使い方を書いていきます。<br>
+            でも、そこまで難しくはないです。ブログidの設定をして、blogs.xmlにブログの情報を書き込むだけです。<br>
+          </p>
+          <p>
+            ね、簡単でしょう?
+          </p>
+        </div>
+        <!--ここまで本文-->
+
+
+
+        <?php include $webroot."/template/blog/after.php" ?>
       </div>
-      <!--ここまで本文-->
 
-
-
-      <?php include $webroot."/template/blog/after.php" ?>
     </div>
   </body>
 </html>
