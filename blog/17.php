8714f9731602c693f3340e578d884d1ca57e6e95
shunby <taby@outlook.jp>
2018-01-09 04:10:02 +0900

    ブログ17

---
+++ blog/17.php
@@ -0,0 +1,74 @@
+<?php
+  $webroot = $_SERVER['DOCUMENT_ROOT'];
+  //include $webroot."/template/check_login.php"
+ ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>だんご三兄弟</title>
+
+    <meta name="description" content="">
+    <meta name="author" content="だんご三兄弟">
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width">
+
+    <?php include "../template/analytics.html" ?>
+    <link href="/template/blog/blog.css" rel="stylesheet" type="text/css">
+    <link href="/template/header.css" rel="stylesheet" type="text/css">
+    <link href="/template/footer.css" rel="stylesheet" type="text/css">
+    <link href="/template/sidemain.css" rel="stylesheet" type="text/css">
+    <link href="/template/content.css" rel="stylesheet" type="text/css">
+    <link href="/template/navi.css" rel="stylesheet" type="text/css">
+    <link href="/template/iine/ajax.css" rel="stylesheet"  type="text/css">
+    <link rel="stylesheet" href="/template/sideber.css" type="text/css">
+    <link href="" rel="shortcut icon">
+    <!--[if lt IE 9]>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+  </head>
+  <body>
+    <div id="content">
+      <?php
+        $webroot = $_SERVER['DOCUMENT_ROOT'];
+        include $webroot."/template/header.html"
+      ?>
+      <?php include $webroot."/template/navi.html" ?>
+      <?php include $webroot."/template/sideber.php" ?>
+
+      <div id="sidemain">
+        <?php include $webroot."/template/blog/before.php" ?>
+
+        <?php
+          $id = 17;
+         ?>
+
+        <!--ここから本文-->
+        <div class="main-main">
+          <p>
+            ねこです。只今の時刻は、午前 3時58分です。<br>
+            朧げな記憶と嘘とを混ぜ込んだ日記を書き、本棚にあった技術書を適当に二冊引き抜いてなんとなく紹介文を書き、
+            無駄に大きい紙を半分に切って「無限の可能性」を書き、今こうしてブログを書いているわけです。うぇーい。
+          </p>
+          <p>
+            おやすみなさい。ねこでした。
+          </p>
+          <p>
+            P.S.<br>
+            おらふの名誉の為に付け加えておくと、情報局第二話は１月８日に執筆されています。<br>
+            誰もサーバーに反映させていなかっただけです。おやすみなさい。
+          </p>
+        </div>
+        <!--ここまで本文-->
+
+
+
+        <?php include $webroot."/template/blog/after.php" ?>
+      </div>
+
+    </div>
+  </body>
+</html>
