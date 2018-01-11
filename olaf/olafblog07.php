eaaac2480cc09442f0a58e6159c2ffa326bbffd5
iamolaf <kkitagawa0212@gmail.com>
2018-01-11 22:48:44 +0900

    aa

---
+++ olaf/olafblog07.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>だんご三兄弟</title>
+
+  <meta name="description" content="">
+  <meta name="author" content="だんご三兄弟">
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <?php
+    $webroot = $_SERVER['DOCUMENT_ROOT'];
+    include $webroot."/template/analytics.html"
+   ?>
+  <link href="index.css" rel="stylesheet" type="text/css">
+  <link href="/template/header.css" rel="stylesheet" type="text/css">
+  <link href="/template/footer.css" rel="stylesheet" type="text/css">
+  <link href="/template/sidemain.css" rel="stylesheet" type="text/css">
+  <link href="/template/content.css" rel="stylesheet" type="text/css">
+  <link href="/template/navi.css" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="/template/sideber.css" type="text/css">
+  <link href="" rel="shortcut icon">
+  <!--[if lt IE 9]>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
+  <![endif]-->
+</head>
+<body>
+
+
+  <div id="content">
+    <?php include $webroot."/template/header.html" ?>
+    <?php include $webroot."/template/navi.html" ?>
+    <?php include $webroot."/template/sideber.php" ?>
+    <main id="sidemain">
+      <p>
+        どうも皆さんお待ちかねのおらふのブログのお時間です‼<br>
+        先日は<s>ゲームやってて</s>忙しくて更新できずすみませんでしたぁ<br>
+        国駿とかねこが俺のPC関係の査定してたけどあれですねだいぶ値段違いますね<br>
+        デバイス関係なんて2万ぐらい多く見積もられてたしそもそも3年だしｗ<br>
+        そんなこんなでこちらが俺のパソコン<br>
+        部屋暗くするとめちゃくちゃきれいですねえ<br>
+        <img src="olafsimage/S_7294141118993．jpg"><br>
+        ちなみに一番こだわったのは外枠の部分でして、razerっていうメーカーの限定販売で今のところ
+        秋葉原のツクモ電気でしかお目にかからないケースです<br>
+        外観を重視したデザインだったので組み立てが大変でした<br>
+        <s>そもそも説明書が英語とか舐めてますよね</s><br>
+      </p>
+      <p>
+        じゃあ今回は宿題の発表です<br>
+        17件もの応募をいただきまして、選考はかなり<s>めんどくさかった</s>悩んだのですが、
+        最優秀賞と優秀賞の発表です<br>
+      </p>
+      <p>
+        <h3>最優秀賞</h3>
+        PN:ウラ嵐マニア　回答：おらふのブログが何ですぐに更新されないかって？更新に時間がかかるからだよ！<br>
+        涙が出るほど感激しました。俺のことを考えてくれてるなんてえええええ‼‼‼‼<br>
+      </p>
+      <p>
+        <h3>優秀賞</h3>
+        PN：あいうえおんぴ　回答：えびらが、えびらがアアアアアァァァァァァ
+        意味不明です。こういう意味不明なの好きです。<Br>
+        </p>
+        <p>
+          どうだったでしょうか。入賞された2人にはなんかあげます。<br>
+          意味不明なのを選ぶと宿題のコーナーそのものが壊滅するのでは、と反省してますｗ<br>
+          次回の宿題はもうちょっと待ってください。近日中には出します。
+        </p>
+
+      </body>
+      </html>
