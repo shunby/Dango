7646a2d2ac46936fb6a2206531d2ff2b649b25e8
shunby <taby@outlook.jp>
2018-01-21 01:28:41 +0900

    wolafblog01

---
+++ olaf/wolafblog01.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>だんご三兄弟</title>
+
+  <meta name="description" content="">
+  <meta name="author" content="だんご三兄弟">
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <?php
+    $webroot = $_SERVER['DOCUMENT_ROOT'];
+    include $webroot."/template/analytics.html"
+   ?>
+  <link href="index.css" rel="stylesheet" type="text/css">
+  <link href="/template/header.css" rel="stylesheet" type="text/css">
+  <link href="/template/footer.css" rel="stylesheet" type="text/css">
+  <link href="/template/sidemain.css" rel="stylesheet" type="text/css">
+  <link href="/template/content.css" rel="stylesheet" type="text/css">
+  <link href="/template/navi.css" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="/template/sideber.css" type="text/css">
+  <link href="" rel="shortcut icon">
+  <!--[if lt IE 9]>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
+  <![endif]-->
+</head>
+<body>
+
+
+  <div id="content">
+    <?php include $webroot."/template/header.html" ?>
+    <?php include $webroot."/template/navi.html" ?>
+    <?php include $webroot."/template/sideber.php" ?>
+    <main id="sidemain">
+      <h1>ヲラフのブログ　第一回</h1>
+      <p>
+        どうもミナさまはじめまして！ワタクシ、運営新メンバーであるところの<a href="/user/?id=1">ヲラフ</a>でございます。<br>
+        さあついに始まりました、ワタクシヲラフが不定期に更新していくコーナー、「ヲラフの日記帳」！
+      </p>
+      <p>
+        記念すべき第一回目、まずはこの新コーナー「ヲラフの日記帳」が始まった経緯やいきさつをお話ししたいと思います。<br>
+      </p>
+      <p>
+        <h2>前任者の失踪</h2>
+        はい、タイトルが思いっきりネタバレしてしまっているのですが、なんと「おらふのブログ」執筆者のおらふ氏が失踪してしまったのです。<br>
+        理由は忙しいから、眠かったから、ゲームしてたから、Minecraftの鯖立てようとして尽く失敗してたから、等諸説ありますが、詳しい詳細はわかっていません。<br>
+        ともかく、前任者のおらふ氏が失踪してしまったのです。これに困ってしまった運営陣は、万能の呼び声高いワタクシに声をかけました。<br>
+        これに、万能な上に性格も良いことに定評のあるワタクシが二つ返事で答えた、というワケです。
+      </p>
+      <p>
+        前任おらふは途中で失敗するような無能ニートでしたが、ワタクシヲラフは読者諸氏の期待に応え、必ずや完璧な幸福へと導くことをお約束しましょう。
+      </p>
+      <p>
+        <h2>「ヲラフの日記帳」について</h2>
+        続いて当コーナーについて。
+        当コーナーでは、前任者の仕事を引き継ぎ「ヲラフの日常」「ヲラフの情報局」を不定期更新していく予定であります。<br>
+        毎日更新にしたいのは山々なのですが、ワタクシは万能者であるが故に皆に頼られてしまい、非常に多忙で忙しさに忙殺し殺されているのでございます、どうかご了承ください。
+        その代わり、最高品質の記事をミナさまにお届けしていく所存でございます。
+      </p>
+      <p>
+        <h2>今回の宿題</h2>
+        それでは最後に、ワタクシからミナさまに今週の宿題を出してお別れしたいと思います。<br>
+        万能であるところのワタクシの最初の宿題は、ズヴァリ！「来週の宿題を考えてください」でございます！<br>
+        それではミナさま、また来週！
+      </p>
+      <p>
+        <br><br><br>
+        <span style="color:rgb(186, 186, 186)">この文章書くの割と疲れる</span>
+      </p>
+
+      </body>
+      </html>
