3f8749709c6797b75884f6653ff8baf36466760f
shunby <taby@outlook.jp>
2018-01-22 19:03:46 +0900

    comment: Accessでローカルでも使えるように対応

--- comment.php
+++ comment.php
@@ -1,19 +1,19 @@
 <div style="font-size:30px; text-align:center">
   <?php
-  $postnum = $_POST['postnum'];
+  require_once($_SERVER['DOCUMENT_ROOT']."/bbs/access/access.php");
   require_once($_SERVER['DOCUMENT_ROOT'].'/core/user_util.php');
+
   session_start();
+
+  $postnum = $_POST['postnum'];
   $error = '';
   if (@$_POST['submit']) {
-    # code...
     if ($_POST['content'] == $error) {
-      # code...
       echo "コメントがありません";
     } else {
-      # code...
       $userid = $_SESSION['user']->id;
       $username = $_SESSION['user']->name;
-      $pdo = new PDO("mysql:dbname=paysestbbs_bbs;charset=utf8;","root");
+      $pdo = Access::getPDO("bbs");
       $set = $pdo->prepare("INSERT INTO comment(postnum,content,userid,username) VALUES(?,?,?,?)");
       $set->execute(array(
       $_POST['postnum'],//postnum
