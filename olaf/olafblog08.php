53953cf6e773eb1e68aa3cfca68a2c24b9d87243
iamolaf <kkitagawa0212@gmail.com>
2018-01-23 23:10:23 +0900

    a

---
+++ olaf/olafblog08.php
@@ -0,0 +1,77 @@
+
+<html>
+<head>
+  <title>だんご三兄弟</title>
+
+  <meta name="description" content="">
+  <meta name="author" content="だんご三兄弟">
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <?php
+    $webroot = $_SERVER['DOCUMENT_ROOT'];
+    include $webroot."/template/analytics.html"
+   ?>
+  <link href="index.css" rel="stylesheet" type="text/css">
+  <link href="/template/header.css" rel="stylesheet" type="text/css">
+  <link href="/template/footer.css" rel="stylesheet" type="text/css">
+  <link href="/template/sidemain.css" rel="stylesheet" type="text/css">
+  <link href="/template/content.css" rel="stylesheet" type="text/css">
+  <link href="/template/navi.css" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="/template/sideber.css" type="text/css">
+  <link href="" rel="shortcut icon">
+  <!--[if lt IE 9]>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
+  <![endif]-->
+</head>
+<body>
+
+
+  <div id="content">
+    <?php include $webroot."/template/header.html" ?>
+    <?php include $webroot."/template/navi.html" ?>
+    <?php include $webroot."/template/sideber.php" ?>
+    <main id="sidemain">
+      <p>
+        どうも皆さんお待ちかね（？）のおらふのブログのお時間です‼<br>
+        ヲラフに乗っ取られかけましたが、無断休載が続きまして、えらいすんまへん。<br>
+        まあ休んでたのには理由がありまして、それを今から話そうかなーと思ってます。<br>
+        このサイトもなんやかんやひと段落ついて、利用者もなんか減ってきて、
+        暇になったので新規プロジェクトを始めようと思い立ったおらふ君。<br>
+        そんなある日おらふ君はYouTubeをみていたら、見つけてしまったのです。<br>
+        <a href="https://www.youtube.com/watch?v=V-waDaPn5h4&t=356s">この動画</a>を。<br>
+        そしてなんとねこさんがマイクラをやっていたことを知ったおらふはこう思ったのです。<br>
+        「ねことマルチで都市を作ろう。」と。<br>
+        そうしてここ最近はマルチ環境の整備をしていたのです。<br>
+        そもそも皆さんが思い浮かべるゲームのマルチとは、パ〇ドラやモ〇ストのように
+        ゲーム内画面で「マルチプレイ」とかの画面から…というのを思い浮かべると思います。<br>
+        これは一般にはゲーム会社のサーバーにアクセスして遊ぶ、という形になるのですが、
+        マイクラをはじめとするPCゲームの中には、ユーザーが自らサーバーを立てないといけないことがあるのです。<br>
+        ＜警告＞ここから結構難しい話になります。頭の弱い方はブラウザをバック‼‼<br>
+        ～マイクラサーバーの立て方～<br>
+        まずは<a href="https://minecraft.net/ja-jp">マイクラ公式サイト</a>からマインクラフトのサーバーをダウンロードします。
+        これは主に多人数で遊ぶ時にいろいろ管理したりします。<br>
+        この後サーバーを起動させるためにちょっとだけいろいろやります<br>
+      　次にやることがポート開放です。これが一番大変です。<br>
+      　そもそもポート開放とはポートを開くことなんですが、最初にざっくりしたイメージだけ言うと、
+      　「ポート」という扉がたくさんあって、マイクラのために「25565」番の扉を開く、という感じです。<br>
+      　ポート開放をする理由は、複数のPC同士で特定のアプリケーションを使って通信をするとき、セキュリティのため
+      　通信がブロックされるためです。<br>
+      　一般的には<br>
+      　・ルーター<br>
+       ・ファイアウォール<br>
+       が通信をブロックします。<br>
+       しかしポート開放をするということは多少の危険を伴います。<br>
+       セキュリティを解除するため開けた扉からはIPアドレスというものを知った人は全員アクセスできてしまうからです。<br>
+       長々とつまらない＆何かがおかしい文を書きましたが、ここ数日おらふ君はポート開放ができなくて困っていたのです。<br>
+       余談ですが、昨日とあるネズミの国の海の方に行ってきまして、買ったグッズの袋をみて思いました。<br>
+       赤丸がおらふ、青丸がヲラフでしょう。<br>
+           <img src="olafsimage/あいうえお.jpg"><br>
+      ではまたお会いしましょう<br>
+    </p>
+
+  </body>
+  </html>
