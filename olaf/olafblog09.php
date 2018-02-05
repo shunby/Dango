9ade79dfd4e40a071ae009ffcc81cb2ad436e357
iamolaf <kkitagawa0212@gmail.com>
2018-02-06 00:18:52 +0900

    あああ

---
+++ olaf/olafblog09.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>だんご三兄弟</title>
+
+  <meta name="description" content="">
+  <meta name="author" content="だんご三兄弟">
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <?php
+    $webroot = $_SERVER['DOCUMENT_ROOT'];
+    include $webroot."/template/analytics.html"
+   ?>
+  <link href="index.css" rel="stylesheet" type="text/css">
+  <link href="/template/header.css" rel="stylesheet" type="text/css">
+  <link href="/template/footer.css" rel="stylesheet" type="text/css">
+  <link href="/template/sidemain.css" rel="stylesheet" type="text/css">
+  <link href="/template/content.css" rel="stylesheet" type="text/css">
+  <link href="/template/navi.css" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="/template/sideber.css" type="text/css">
+  <link href="" rel="shortcut icon">
+  <!--[if lt IE 9]>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
+  <![endif]-->
+</head>
+<body>
+
+
+  <div id="content">
+    <?php include $webroot."/template/header.html" ?>
+    <?php include $webroot."/template/navi.html" ?>
+    <?php include $webroot."/template/sideber.php" ?>
+    <main id="sidemain">
+      <p>
+        どうも皆さんお待ちかね（？）のおらふのブログのお時間です‼<br>
+        最近このサイトが暇になってきたので他のことをやろうと思って<br>
+        YouTubeへの動画投稿をすることに決定いたしました‼‼<br>
+        運営メンバーの卍共和国は前からちょっとやってたんですが、ねことおらふでやろうという話になって。<br>
+        それで2人でマイクラやってます。<br>
+        そんなこんなで今日、2月5日の夜23時29分、おらふはすでに2時間半パソコンをやっていた疲れ目を休めつつ
+        仕事をするのであった…<br>
+        やらなきゃいけないことはマイクラの動画素材の録画と編集、ブログの更新なんですけど、
+        録画はおわってブログも今やってるので後は編集だけですね<br>
+        基本的にゆっくりムービーメーカー、通称YMMで編集してるんですけど、これがまた難しくて。<br>
+        俺は今日が初めてなんですけどかなりめんどくさいですね、はい。<br>
+        ゆっくり実況者さんの更新ペースのことをとやかく言えなくなりましたよｗ<br>
+        俺はパソコンゲームが大好きなんですけど、最近マイクラしかやってないからBF4もやりたいな、とか。<br>
+        ところで皆さん勉強中に音楽とか聴きますか？<br>
+        俺は割とよく聴くんですが、勉強にかかわらず作業の時はよく聴きますね<br>
+        いまもTHE BLUE HEARTS聴きながらやってるんですが、これがまたいいの。<br>
+        ブルーハーツってのは20年ぐらい前に若者からの絶大な人気を得た伝説的なバンドなんですが、
+        「リンダリンダ」歌ってるバンドっていったらわかりますかね？<br>
+        ボーカルの歌い方はかなり衝撃を受けましたけど、俺は歌詞がとっても気に入りました。<br>
+        今どきよくある「君が好きだ！」とか「君以外考えられない！」みたいななよなよした恋の歌じゃなくて、
+        若者の心情を的確に表現した歌なんですね、<br>
+        気が向いたらこんど歌詞でも上げてみます。<br>
+        ちなみにおすすめの歌は「リンダ　リンダ」「TRAIN-TRAIN」「夢」「旅人」
+        「月の爆撃機」あたりでしょうか<br>
+        それでは今回はここらへんで。また動画アップしたらお知らせします。<br>
+      </p>
+
+    </body>
+    </html>
