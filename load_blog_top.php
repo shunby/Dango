6b05616fab42b74dee08db6edd5785ad83086c73
shunby <taby@outlook.jp>
2018-03-05 01:21:41 +0900

    ブログトップ読み込みからスクリプト実行可能に

--- load_blog_top.php
+++ load_blog_top.php
@@ -5,7 +5,7 @@ $(function(){
     datatype: 'html'
   }).then(
   function(data){
-    var out = $($.parseHTML(data));//ブログのhtml全体
+    var out = $($.parseHTML(data, true));//ブログのhtml全体
     $(".blog_content").append($("<h1>"+blog_title+"</h1>").css({'text-align':'center'}));
     $(".blog_content").append($("<div>"+blog_date+"</div>").css({'text-align':'right', 'font-size':'0.8em', 'margin-bottom': '30px', 'padding-right': '20px'}));
     $(".blog_content").append(out.find(".main-main")[0].outerHTML);
