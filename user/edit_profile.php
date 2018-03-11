32e815719ffd548e85a86036ea58fc619c5d645c
shunby <taby@outlook.jp>
2018-03-11 16:02:09 +0900

    ひらがなも打てる新自己紹介エディタ

--- user/edit_profile.php
+++ user/edit_profile.php
@@ -34,6 +34,7 @@
   <?php
     include $webroot."/template/analytics.html"
    ?>
+  <link href="/user/edit_profile.css" rel="stylesheet" type="text/css">
   <link href="/template/header.css" rel="stylesheet" type="text/css">
   <link href="/template/footer.css" rel="stylesheet" type="text/css">
   <link href="/template/main.css" rel="stylesheet" type="text/css">
@@ -42,7 +43,7 @@
 
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.css">
   <script src="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.js"></script>
-
+  <script src='/marked.js'></script>
   <link href="" rel="shortcut icon">
   <!--[if lt IE 9]>
   <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
@@ -55,63 +56,95 @@
     <?php include $webroot."/template/header.html" ?>
     <?php include $webroot."/template/navi.html" ?>
     <main id="main">
-      <div>
+      <h1>自己紹介を編集</h1>
+      <p id="tabcontrol">
+        <a href="#tabpage_edit">編集</a>
+        <a href="#tabpage_preview">プレビュー</a>
+      </p>
+      <div id="tabbody">
+        <div id="tabpage_edit">
 
-        <h1>自己紹介を編集</h1>
-        <form method="post" action="/user/edit_profile.php" name="profile_form" onsubmit="return checkTextarea();">
-          <textarea id="editor" name="profile" style="width: 100%;height: 100em;font-size: 1.2em"></textarea>
-          <input type="submit" value="投稿"></input>
-        </form>
-        <script>
-          <?php
-            $pdo = Access::getPDO('bbs');
-            $sql = 'SELECT * from user where id=?';
-            $getprof_stmt = $pdo->prepare($sql);
-            $getprof_stmt->execute(array($_SESSION['user']->id));
 
-            $profile = $getprof_stmt->fetch()['profile'];
-            $profile = json_encode(htmlspecialchars_decode($profile));
-            echo <<<EOM
-            var profile={$profile};
+          <form method="post" action="/user/edit_profile.php" name="profile_form" onsubmit="return checkTextarea();">
+            <textarea id="editor" name="profile" style="width: 100%;height: 100em;font-size: 1.2em"></textarea>
+            <input type="submit" value="投稿"></input>
+          </form>
+          <script>
+            <?php
+              $pdo = Access::getPDO('bbs');
+              $sql = 'SELECT * from user where id=?';
+              $getprof_stmt = $pdo->prepare($sql);
+              $getprof_stmt->execute(array($_SESSION['user']->id));
+
+              $profile = $getprof_stmt->fetch()['profile'];
+              $profile = json_encode(htmlspecialchars_decode($profile));
+              echo <<<EOM
+              var profile={$profile};
 EOM;
-          ?>
-          var ua = navigator.userAgent;
-          var ios = ua.indexOf("iOS") != -1;
-          var simplemde;
-          if(!ios){//iOS以外
-            //textareaにSimpleMDEのスタイルを適用
-            simplemde = new SimpleMDE({ element: document.getElementById("editor") });
-            simplemde.value(profile);
-
-          }else{//iOSは地のtextareaを表示
-            profile_form.profile.value = profile;
-          }
+            ?>
+            document.profile_form.profile.value = profile;
 
 
-          //送信時に内容を確認 1文字以上5000文字以内ならTrueを返す
-          function checkTextarea(){
-            var text_length;
-            if(!iOS){
-              text_length = simplemde.value().length;
-            }else{
-              text_length = profile_form.profile.value.length;
+
+            //送信時に内容を確認 1文字以上5000文字以内ならTrueを返す
+            function checkTextarea(){
+              var text_length = profile_form.profile.value.length;
+              
+              if(text_length < 1){
+                alert("1文字以上入力してください。");
+                return false;
+              }else if(text_length > 5000){
+                alert("5000文字以内で入力してください。");
+                return false;
+              }else{
+                return true;
+              }
             }
+          </script>
 
+        </div>
+        <div id="tabpage_preview">
 
-            if(text_length < 1){
-              alert("1文字以上入力してください。");
-              return false;
-            }else if(text_length > 5000){
-              alert("5000文字以内で入力してください。");
-              return false;
-            }else{
-              return true;
-            }
-          }
-        </script>
-
+        </div>
       </div>
+
     </main>
   </div>
-</body>
+  <script type="text/javascript">
+    var tabs = document.getElementById('tabcontrol').getElementsByTagName('a');
+    var pages = document.getElementById('tabbody').getElementsByTagName('div');
+
+    function changeTab() {
+      //リンクの#以降の文字列
+       var targetid = this.href.substring(this.href.indexOf('#')+1,this.href.length);
+
+       for(var i=0; i<pages.length; i++) {
+          if( pages[i].id != targetid ) {
+             pages[i].style.display = "none";
+          }
+          else {
+             pages[i].style.display = "block";
+          }
+       }
+
+       for(var i=0; i<tabs.length; i++) {
+          tabs[i].style.zIndex = "0";
+       }
+       this.style.zIndex = "10";
+
+       var prof = document.profile_form.profile.value;
+       prof = marked(prof);
+       prof = prof.replace(/\r?\n/g, '<br>');
+       document.getElementById("tabpage_preview").innerHTML = prof;
+
+       return false;
+    }
+
+    for(var i=0; i<tabs.length; i++) {
+       tabs[i].onclick = changeTab;
+    }
+
+    tabs[0].onclick();
+    </script>
+  </body>
 </html>
